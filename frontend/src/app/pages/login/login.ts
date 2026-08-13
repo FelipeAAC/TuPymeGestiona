cb01b48cf28318d738b2f3f8e4e2efd292952636
FelipeAAC <fe.arcos@duocuc.cl>
2026-08-13 17:16:53 -0400

    Add login form behavior

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+
+  showPassword = false;
+
+  readonly loginForm = this.formBuilder.group({
+    identifier: ['', Validators.required],
+    password: ['', Validators.required],
+    rememberMe: [false],
+  });
+
+  get identifier() {
+    return this.loginForm.controls.identifier;
+  }
+
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+  }
+}
